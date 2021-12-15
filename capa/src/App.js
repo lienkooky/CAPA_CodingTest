739c82cd9586843e86e95d84a192c4d7507cc6ee
GEONYEONG SON <thsrjs5@gmail.com>
2021-12-15 19:21:42 +0900

    settings

---
+++ capa/src/App.js
@@ -0,0 +1,13 @@
+import Header from './View/header';
+// import Main from './View/main';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      {/* <Main /> */}
+    </div>
+  );
+}
+
+export default App;
