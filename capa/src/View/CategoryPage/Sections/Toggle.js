95151f2639fddd353ab34291e52a3f15d018d5f7
GEONYEONG SON <thsrjs5@gmail.com>
2021-12-17 15:12:20 +0900

    Add: toggle

--- capa/src/View/CategoryPage/Sections/Toggle.js
+++ capa/src/View/CategoryPage/Sections/Toggle.js
@@ -4,33 +4,41 @@ import styled from 'styled-components';
 const ToggleContainer = styled.div`
   display: flex;
   align-items: center;
+  justify-content: center;
   position: relative;
   cursor: pointer;
   > .toggle-container {
-    width: 50px;
-    height: 24px;
+    width: 40px;
+    height: 14px;
     border-radius: 30px;
-    background-color: #8b8b8b;
+    background-color: #c2c2c2;
     transition: all 0.2s ease;
+
     &.toggle--checked {
-      background-color: #4000c7;
+      background-color: #badefb;
     }
   }
   > .toggle-circle {
     position: absolute;
-    top: 1px;
-    left: 1px;
-    width: 22px;
-    height: 22px;
+    top: 2px;
+    left: 0px;
+    width: 20px;
+    height: 20px;
     border-radius: 50%;
-    background-color: #fafafa;
+    background-color: #fff;
+    border: 1px solid #eee;
+    box-shadow: 1px 1px 1px rgba(0, 0, 0, 0.5);
     transition: all 0.25s ease;
+    box-sizing: border-box;
     &.toggle--checked {
-      left: 27px;
+      left: 17px;
+      background-color: #2096f3;
+      border: 1px solid #2096f3;
     }
   }
   > span {
     margin-left: 20px;
+    font-size: 14px;
   }
 `;
 
